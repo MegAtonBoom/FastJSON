9a9a3a56e8eb7b35bb4457edede6382a22b5c2c9
MegAtonBoom <baba.adrian13@gmail.com>
2022-06-25 20:56:35 +0200

    commit #11: several test fixes, added test 2, maybe last commit?

--- src/test/java/JSONArrayParseTest.java
+++ src/test/java/JSONArrayParseTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.alibaba.fastjson.JSONException;
-import junit.framework.TestCase;
 import org.junit.Assert;
 
 
@@ -19,8 +18,11 @@ import org.junit.runners.Parameterized;
 public class JSONArrayParseTest extends junit.framework.TestCase {
 
     private String text;
-    private List<Map<String, Integer>> array;
+    private List<Map<String, Integer>> list;
     private Map<String, Integer> map;
+
+    boolean correctFormat;
+    boolean hasBracket;
 
     public JSONArrayParseTest(String text){
         configure(text);
@@ -28,52 +30,141 @@ public class JSONArrayParseTest extends junit.framework.TestCase {
 
     public void configure(String text){
         this.text=text;
+
+
+        if(text!=null) {
+            this.text = this.text.replaceAll("\\s", "");
+        }
+        this.correctFormat=correctFormat();
+
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(new Object[][] {
-                {"[{id3:123l"},{"p{i:178}]"}
+
+                //parameter string
+                {"[{i:123}]"},
+                {null},
+                {"[{id3:123l"},
+                {"[{id3:123, id5:321}]"},
+
         });
     }
 
-    @org.junit.Test
+    @Test
     public void testArray() throws Exception {
-        int pos=this.text.indexOf(":"), endPos=this.text.length()-2, i, startPos=this.text.indexOf("{");
-        String idString, idNameString;
-        char[] id, idName;
-        try {
-            idName = new char [pos-startPos-1];
-            idNameString=String.valueOf(idName);
-            this.array=JSON.parseObject(text, new TypeReference<List<Map<String, Integer>>>() {});
-            System.out.println(array+"    "+this.array.get(0));
-            this.map  = this.array.get(0);
-            id = new char[endPos-pos];
-            this.text.getChars(pos+1, endPos, id, 0);
-            idString = String.valueOf(id);
-            i = Integer.parseInt(idString);
-            Assert.assertEquals(1, this.array.size());
-            Assert.assertEquals(i, this.map.get(idNameString).intValue());
-        }
-        catch(Exception e) {
-            boolean correct=true;
-            if (this.text.indexOf("[")!=0||this.text.indexOf("{")!=1) correct=false;
-            else if((this.text.substring(endPos))!="}]") correct=false;
-            else if(pos==-1) correct=false;
-            else{
-                try{
-                    id = new char[endPos-pos];
-                    this.text.getChars(pos+1, endPos, id, 0);
-                    idString = String.valueOf(id);
-                    i = Integer.parseInt(idString);
-                }
-                catch(NumberFormatException ne){
-                    correct=false;
-                }
-            }
-            Assert.assertFalse(correct);
 
+        String current=null;
+        if(this.text!=null)
+        current = new String(this.text);
+
+        boolean isFinished=false;
+
+        try{
+            this.list=JSON.parseObject(text, new TypeReference<List<Map<String, Integer>>>() {});
+            this.map  = this.list.get(0);
+            Assert.assertEquals(1, this.list.size());
         }
+        catch(JSONException je){
+            Assert.assertFalse(this.correctFormat);
+            return;
+        }
+        catch(NullPointerException npe){
+            Assert.assertFalse(this.correctFormat);
+            return;
+        }
+        while(!isFinished){
+
+            int pos=current.indexOf(":");
+            int endPos=current.indexOf(",");
+
+            if(endPos==-1) {
+                endPos=current.length()-2;
+                isFinished=true;
+            }
+            int startPos=current.indexOf("{");
+            if(startPos==-1) startPos=0;
+            else startPos++;
+            int i;
+            String idString, idNameString;
+            idNameString=current.substring(startPos, pos);
+            idString=current.substring(pos+1,endPos);
+            if(isFinished)
+                Assert.assertEquals(Integer.valueOf(idString), this.map.get(idNameString));
+            if(!isFinished)
+                current=current.substring(endPos+1);
+        }
+
+
+    }
+
+    private boolean correctFormat(){
+
+        if(this.text==null)
+            return false;
+
+        //boolean firstIter=true;
+        String current=new String(this.text);
+        String value;
+
+
+            int pos1 = this.text.indexOf("[");
+            int pos2 = this.text.indexOf("{");
+            int pos3 = this.text.indexOf("}");
+            int pos4 = this.text.indexOf("]");
+            if (pos1 == -1 || pos2 == -1 || pos4 == -1) return false;
+            if (pos3 == -1) this.hasBracket = false;
+            else this.hasBracket = true;
+
+            int pos6 = 0;
+
+            if (pos2 < pos1) return false;
+            if (pos1 > pos4 || pos2 > pos4) return false;
+            if (this.hasBracket) {
+                if (pos1 > pos3 || pos2 > pos3) return false;
+                if (pos4 < pos3) return false;
+            }
+
+            while (pos6 != -1) {
+
+
+                int pos5 = current.indexOf(":");
+                if (pos5 == -1) {
+                    return false;
+                }
+                pos6 = current.indexOf(",");
+                if (pos6 < pos5 && pos6 != -1) return false;
+
+                if (pos6 != -1) {
+
+                    value = current.substring(pos5 + 1, pos6);
+                    try {
+                        Integer.valueOf(value);
+                    } catch (NumberFormatException nfe) {
+                        return false;
+                    }
+                    current = current.substring(pos6 + 1);
+                } else {
+                    if (this.hasBracket)
+                        value = current.substring(pos5 + 1, current.length() - 2);
+                    else
+                        value = current.substring(pos5 + 1, current.length() - 1);
+                    try {
+                        Integer.valueOf(value);
+                    } catch (NumberFormatException nfe) {
+                        return false;
+                    }
+
+                    break;
+                }
+
+
+            }
+
+
+        return true;
+
     }
 
 
--- src/test/java/Map2Test.java
+++ src/test/java/Map2Test.java
@@ -3,41 +3,98 @@ import com.alibaba.fastjson.TypeReference;
 import junit.framework.TestCase;
 import org.junit.Assert;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.Map;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-//@RunWith(Parameterized.class)
+@RunWith(Parameterized.class)
 public class Map2Test extends TestCase {
 
     private String object;
-    Map<Object, Object> map;
+    private Map<Object, Object> map;
+    private String expectedValue;
+    private dataType expectedValueType;
+    private String expectedKey;
+    private dataType expectedKeyType;
+    private boolean expectedExc;
 
 
-    public Map2Test(){
-        configure("{1:\"2\",\"3\":4,'5':6}");
+    public Map2Test(String object, dataType expectedValueType, String expectedValue, dataType expectedKeyType, String expectedKey, boolean expectedExc){
+        configure(object, expectedValueType, expectedValue, expectedKeyType, expectedKey, expectedExc);
     }
 
-    public void configure(String object){
+    public void configure(String object, dataType expectedValueType, String expectedValue, dataType expectedKeyType, String expectedKey, boolean expectedExc){
+        this.expectedKey = expectedKey;
+        this.expectedKeyType = expectedKeyType;
+        this.expectedValue = expectedValue;
+        this.expectedValueType = expectedValueType;
+        this.expectedExc = expectedExc;
+
         this.object=object;
-        this.map=JSON.parseObject(this.object, new TypeReference<Map<Object, Object>>() {
+
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList(new Object[][] {
+
+                //object                    value type          value       key type            key     expected Exception
+                {"{\"1\":\"2\"}",           dataType.STRING,    "2",        dataType.STRING,    "1",    false},
+                {"{\"1\"\"2\"}",           dataType.STRING,    "2",        dataType.STRING,    "1",    true},
+                {null,                      dataType.INT,       "3",        dataType.INT,       "2",    true},
+                {"{\"1\":\"2\", 2:3}",      dataType.INT,       "3",        dataType.INT,       "2",    false},
+
         });
-
-
     }
 
     @Test
     public void testObject(){
-        Assert.assertEquals("2", map.get(1));
-        Assert.assertEquals(4, map.get("3"));
-        Assert.assertEquals(6, map.get("5"));
+
+        try {
+            this.map = JSON.parseObject(this.object, new TypeReference<Map<Object, Object>>() {
+            });
+
+            if (expectedKeyType == dataType.STRING) {
+                String key = expectedKey;
+                if (expectedValueType == dataType.STRING) {
+                    String res = expectedValue;
+                    Assert.assertEquals(res, map.get(key));
+                    return;
+                } else {
+                    Integer res = Integer.valueOf(expectedValue);
+                    Assert.assertEquals(res, map.get(key));
+                    return;
+                }
+            } else {
+                Integer key = Integer.valueOf(expectedKey);
+                if (expectedValueType == dataType.STRING) {
+                    String res = expectedValue;
+                    Assert.assertEquals(res, map.get(key));
+                    return;
+                } else {
+                    Integer res = Integer.valueOf(expectedValue);
+                    Assert.assertEquals(res, map.get(key));
+                    return;
+                }
+            }
+        }
+        catch(Exception e){
+            Assert.assertTrue(this.expectedExc);
+        }
+
+
     }
 
-    /*@Parameterized.Parameters
-    public static Collection<Object[]> getParameters() {
-        return Arrays.asList(new Object[][] {
-                {"{1:\"2\",\"3\":4,'5':6}"}
-        });
-    }*/
+
+
+    private enum dataType{
+        STRING,
+        INT
+    }
 
 }
